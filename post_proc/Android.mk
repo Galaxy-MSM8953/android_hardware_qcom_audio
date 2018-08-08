bb13366365969ad0ec4b103f6b271ba8f0bfbffa
Nicholas Lim <nicknitewolf@lineageos.org>
2018-08-09 02:45:44 +0800

    Use -Werror in audio-caf/msm8916
    * Suppress existing warnings.
    
    Change-Id: I56089299a3b484e3e643bb1b15eac329975b398c

--- post_proc/Android.mk
+++ post_proc/Android.mk
@@ -1,5 +1,12 @@
 LOCAL_PATH:= $(call my-dir)
 
+qcom_post_proc_common_cflags := \
+    -O2 -fvisibility=hidden \
+    -Wall -Werror \
+    -Wno-unused-function \
+    -Wno-unused-variable \
+    -Wno-sign-compare -Wno-format
+
 include $(CLEAR_VARS)
 
 ifneq ($(strip $(AUDIO_FEATURE_ENABLED_PROXY_DEVICE)),false)
@@ -25,7 +32,7 @@ ifeq ($(strip $(AUDIO_FEATURE_ENABLED_AUDIOSPHERE)),true)
     LOCAL_SRC_FILES += asphere.c
 endif
 
-LOCAL_CFLAGS+= -O2 -fvisibility=hidden
+LOCAL_CFLAGS += $(qcom_post_proc_common_cflags)
 
 ifeq ($(strip $(AUDIO_FEATURE_ENABLED_DTS_EAGLE)), true)
     LOCAL_CFLAGS += -DDTS_EAGLE
@@ -70,7 +77,7 @@ LOCAL_SHARED_LIBRARIES := \
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_CFLAGS += -O2 -fvisibility=hidden
+LOCAL_CFLAGS += $(qcom_post_proc_common_cflags)
 
 ifeq ($(strip $(AUDIO_FEATURE_ENABLED_DTS_EAGLE)), true)
 LOCAL_CFLAGS += -DHW_ACC_HPX
